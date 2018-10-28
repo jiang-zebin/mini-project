c5c0df52cab1a65bcd152e66ed5ac36eac269639
Jiang Zebin <741774490@qq.com>
2018-10-28 19:52:29 +0800

    complete the cart page

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -6,15 +6,80 @@ Page({
    */
   data: {
     district:["艾欧尼亚","祖安","战争学院","雷瑟守备"],
-    index:0
+    index:0,
+    cartlist:[
+      {cid:1,img_url:"/assets/image/big92000.jpg",title:"放逐之刃 锐雯",price:4500,rmb:"45.00",check:false},
+      {cid:2,img_url:"/assets/image/big157000.jpg",title:"疾风剑豪 亚索",price:4500,rmb:"45.00",check:false},
+      {cid:3,img_url:"/assets/image/big238000.jpg",title:"影流之主 劫",price:4500,rmb:"45.00",check:false},
+      {cid:4,img_url:"/assets/image/big92016.jpg",title:"黎明使者 锐雯",price:9900,rmb:"99.00",check:false}
+    ],
+    checkAll:false,
+    priceAll:0,
+    rmbAll:0,
+    count:0
   },
+  //选择大区
   districtChange:function(e){
     this.setData({
-      index: e.detail.value
+      index:e.detail.value
     })
   },
-  show:function(e){
-    console.log(e.detail.value)
+  //购物车列表项被选中时触发
+  selectOne:function(e){
+    var index=e.target.dataset.id-1;
+    this.data.cartlist[index].check=!this.data.cartlist[index].check;
+    if(this.data.cartlist[index].check){
+      this.data.priceAll+=parseInt(this.data.cartlist[index].price);
+      this.data.rmbAll=parseInt(this.data.rmbAll)+parseInt(this.data.cartlist[index].rmb);
+      this.data.count+=1;
+    }else{
+      this.data.priceAll-=parseInt(this.data.cartlist[index].price);
+      this.data.rmbAll=parseInt(this.data.rmbAll)-parseInt(this.data.cartlist[index].rmb);
+      this.data.count-=1;
+    }
+    var sum=0;
+    for(var i=0;i<this.data.cartlist.length;i++){
+      sum+=this.data.cartlist[i].price
+    }
+    if(this.data.priceAll==sum){
+      this.data.checkAll=true;
+    }else{
+      this.data.checkAll=false;
+    }
+    this.setData({
+      cartlist:this.data.cartlist,
+      checkAll:this.data.checkAll,
+      priceAll:this.data.priceAll,
+      rmbAll:this.data.rmbAll.toFixed(2),
+      count:this.data.count
+    });
+  },
+  //全选打勾、计算金额
+  selectAll:function(){
+    var sum=0;
+    if(this.data.checkAll==false){
+      for(var i=0;i<this.data.cartlist.length;i++){
+        sum+=this.data.cartlist[i].price;
+        this.data.cartlist[i].check=true;
+        this.data.priceAll=parseInt(sum);
+        this.data.rmbAll=(parseInt(sum)/100).toFixed(2);
+      }
+      this.data.count=this.data.cartlist.length;
+    }else{
+      for(var i=0;i<this.data.cartlist.length;i++){
+        this.data.cartlist[i].check=false;
+      }
+      this.data.priceAll=0;
+      this.data.rmbAll=0;
+      this.data.count=0;
+    }
+    this.setData({
+      cartlist:this.data.cartlist,
+      checkAll:!this.data.checkAll,
+      priceAll:this.data.priceAll,
+      rmbAll:this.data.rmbAll,
+      count:this.data.count
+    })
   },
   /**
    * 生命周期函数--监听页面加载
